1ccb6756679bbafac6113638e58c33ab3d863b72
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2023-10-14 16:59:42 -0700

    Modified javascript for click event

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-var button = document.querySelector(".show-cat");
-button.addEventListener("click", function () {
-  button.innerText = "It's working.";
-});
-console.log(button);
+//var button = document.querySelector(".show-cat");//
+//button.addEventListener("click", function () {//
+//  button.innerText = "It's working.";//
+//});//
+//console.log(button);//
 
 var cat = document.querySelector(".cat");
 cat.classList.add("show");
 console.log(cat);
+
+var button = document.querySelector(".show-cat");
+console.log(button);
+
+var cat = document.querySelector("figure .cat");
+console.log(cat);
+
+button.addEventListener("click", function () {
+  button.innerText = "It's working."
+})
