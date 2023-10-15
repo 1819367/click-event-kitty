114dad5d02ecf21a85f212f9ee87be680c809536
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2023-10-14 19:07:24 -0700

    Added javascript code for if else statements

--- js/script.js
+++ js/script.js
@@ -14,6 +14,23 @@ console.log(button);
 var cat = document.querySelector(".cat");
 console.log(cat);
 
+//button.addEventListener("click", function () {//
+  //if (cat.classList.contains("show")) {//
+   // console.log("Yes.");//
+//}  else {//
+  //console.log("No.");//
+//}//
+//});//
+
+
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    cat.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+  }
 })
