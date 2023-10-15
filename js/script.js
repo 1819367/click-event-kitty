85957e65dc566a24a36c4c6a85897132ae03e4b8
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2023-10-15 13:46:20 -0700

    Corrected javascript for blue botton to show

--- js/script.js
+++ js/script.js
@@ -8,12 +8,6 @@
 //cat.classList.add("show");//
 //console.log(cat);//
 
-var button = document.querySelector(".show-cat");
-console.log(button);
-
-var cat = document.querySelector(".cat");
-console.log(cat);
-
 //button.addEventListener("click", function () {//
   //if (cat.classList.contains("show")) {//
    // console.log("Yes.");//
@@ -22,13 +16,20 @@ console.log(cat);
 //}//
 //});//
 
+var button = document.querySelector(".show-cat");
+console.log(button);
+
+var cat = document.querySelector(".cat");
+console.log(cat);
 
 button.addEventListener("click", function () {
   if (cat.classList.contains("show")) {
+
     cat.classList.remove("show");
     button.innerText = "Wait, come back!";
-    cat.classList.add("disappear");
+    button.classList.add("disappear");
   } else {
+
     cat.classList.add("show");
     button.innerText = "Shoo, cat!!";
     button.classList.remove("disappear");
